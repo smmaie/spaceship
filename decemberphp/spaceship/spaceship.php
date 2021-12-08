1318e7c00591fe86071dc46b85cc85e016f445f1
smmaie <75067707+smmaie@users.noreply.github.com>
2021-12-08 16:28:14 +0100

    Add files via upload

---
+++ decemberphp/spaceship/spaceship.php
@@ -0,0 +1,17 @@
+<?php
+class spaceship
+{
+    // Propjerties
+    public bool $isalive;
+    public int $fuel;
+    public int $hitpoints;
+    public int $ammo;
+    //constructor
+
+    //methods/functions
+
+    //get/set
+
+}
+
+?>
